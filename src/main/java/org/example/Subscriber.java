495bc7fbfbcb6dc8beb45e71cf0a17ce564665a5
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-04-27 14:21:17 +0100

    included consumer on Subscriber
    The consumer allows the user to view all messages sent

--- src/main/java/org/example/Subscriber.java
+++ src/main/java/org/example/Subscriber.java
@@ -9,18 +9,18 @@
 package org.example;
 
 //To import this I have to included in my Dependency in POM File
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.*;
 
 //The extra DeliverCallback interface we'll use to buffer the messages pushed to us by the server.
-import com.rabbitmq.client.DeliverCallback;
 
 import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeoutException;
 
 public class Subscriber
 {
+
+    private static final CountDownLatch latch = new CountDownLatch(1);
     /**
      * Setting up is the same as the publisher;
      * we open a connection and a channel,
@@ -29,8 +29,7 @@ public class Subscriber
      */
     private final static String QUEUE_NAME = "FirstMQTT";
 
-    public static void main(String[] args) throws IOException, TimeoutException
-    {
+    public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         Connection connection = factory.newConnection();
@@ -43,6 +42,23 @@ public class Subscriber
          */
         channel.queueDeclare(QUEUE_NAME, false, false, false, null);
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+
+        //Create a consumer and bind it to the queue
+        Consumer consumer = new DefaultConsumer(channel)
+        {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println(" [x] Received '" + message + "'");
+            }
+        };
+
+        // Start consuming messages from the queue
+        channel.basicConsume(QUEUE_NAME, true, consumer);
+
+        // Wait indefinitely until notified to exit
+        latch.await();
     }
 
 }

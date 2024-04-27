a11b2d4ce75c4ad9dbae9498eddefc1e4a5305f4
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-04-27 14:03:55 +0100

    created a method sayHello as example
    The example created is just to understand the idea
    All information was obtained from Rabbit MQ

--- src/main/java/org/example/Publisher.java
+++ src/main/java/org/example/Publisher.java
@@ -1,49 +1,113 @@
+/**
+ * This is my first MQTT using RabbitMQTT
+ * Author: Sergio Oliveira
+ * Distributed System - 27 April 2024
+ *
+ * RabbitMQ tutorial:
+ *  https://www.rabbitmq.com/tutorials/tutorial-one-java
+ */
+
+
 package org.example;
 
-import java.util.ArrayList;
-import java.util.concurrent.CountDownLatch;
-
-import com.rabbitmq.client.Channel; //to import this I have to included in my Dependency in POM File
-
+//To import this I have to included in my Dependency in POM File
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 
 public class Publisher
 {
 
-    private static int temperature;
-    private static ArrayList<String> cities = new ArrayList<>(); // ArrayList to store the name of the cities
 
+    private static String QUEUE_NAME = "Hello"; //Set up the class and name the queue:
+    //private static ArrayList<String> cities = new ArrayList<>(); // ArrayList to store the name of the cities
 
 
     public static void main(String[] args)
     {
+
+
+        sayHello();
+
+
+
         //Add cities into ArrayList
-        cities.add("Dublin");
-        cities.add("Swords");
-        cities.add("Donabate Beach");
+        //cities.add("Dublin");
+        //cities.add("Swords");
+        //cities.add("Donabate Beach");
 
         // Publish transactions with a delay
 
 
+
+
+    }
+
+    private static void sayHello()
+    {
+        /**
+         * The connection abstracts the socket connection,
+         * and takes care of protocol version negotiation
+         * and authentication and so on for us.
+         * Here we connect to a RabbitMQ node on the local machine - hence the localhost.
+         * If we wanted to connect to a node on a different machine
+         * we'd simply specify its hostname or IP address here.
+         */
+
+        //Create a connection to the server:
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost"); // RabbitMQ server host
+
+        /**
+         * Next we create a channel, which is where most of the API
+         * for getting things done resides.
+         * Note we can use a try-with-resources statement
+         * because both Connection and Channel implement java.lang.AutoCloseable.
+         * This way we don't need to close them explicitly in our code.
+         */
+
+        /**
+         * To send, we must declare a queue for us to send to;
+         * then we can publish a message to the queue,
+         * all of this in the try-with-resources statement:
+         */
+
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel();)
+        {
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            String message = "Hello World!";
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+            System.out.println(" [x] Sent '" + message + "'");
+
+        }
+        catch (Exception e)
+        {
+            System.out.println("If there is something is wrong");
+        }
+
     }
 
 
-    /*
 
+    /*
     private static void temperatureGenerator()
     {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost"); // RabbitMQ server host
 
         try (Connection connection = factory.newConnection();
-             Channel channel = connection.createChannel()) {
+             Channel channel = connection.createChannel())
+        {
             // Declare the queue
             channel.queueDeclare(QUEUE_NAME, false, false, false, null);
 
             // Publish each transaction in the ArrayList to the queue with a delay
-            for (String transaction : transactions) {
-                channel.basicPublish("", QUEUE_NAME, null, transaction.getBytes());
-                System.out.println(" [x] Sent '" + transaction + "'");
-                Thread.sleep(15000); // Delay in milliseconds (15 seconds)
+            for (String myCity : cities)
+            {
+                channel.basicPublish("", QUEUE_NAME, null, myCity.getBytes());
+                System.out.println(" [x] Sent '" + cities + "'");
+                Thread.sleep(1000); // Delay in milliseconds (15 seconds)
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -51,4 +115,6 @@ public class Publisher
     }
 
      */
+
+
 }
--- src/main/java/org/example/Subscriber.java
+++ src/main/java/org/example/Subscriber.java
@@ -1,5 +1,48 @@
+/**
+ * This is my first MQTT using RabbitMQTT
+ * Author: Sergio Oliveira
+ * Distributed System - 27 April 2024
+ *
+ * RabbitMQ tutorial:
+ *  https://www.rabbitmq.com/tutorials/tutorial-one-java
+ */
 package org.example;
+
+//To import this I have to included in my Dependency in POM File
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+//The extra DeliverCallback interface we'll use to buffer the messages pushed to us by the server.
+import com.rabbitmq.client.DeliverCallback;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 
 public class Subscriber
 {
+    /**
+     * Setting up is the same as the publisher;
+     * we open a connection and a channel,
+     * and declare the queue from which we're going to consume.
+     * Note this matches up with the queue that send publishes to.
+     */
+    private final static String QUEUE_NAME = "Hello";
+
+    public static void main(String[] args) throws IOException, TimeoutException
+    {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        /**
+         * Note that we declare the queue here, as well.
+         * Because we might start the consumer before the publisher,
+         * we want to make sure the queue exists before we try to consume messages from it.
+         */
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+    }
+
 }

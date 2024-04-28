04c0cd9554249c31b1f78b2bb5088c76955a0b4b
sergio-oliveira-br <142422148+sergio-oliveira-br@users.noreply.github.com>
2024-04-28 23:26:31 +0100

    Update SecondSubscriber.java

--- src/main/java/org/example/SecondSubscriber.java
+++ src/main/java/org/example/SecondSubscriber.java
@@ -30,20 +30,13 @@ public class SecondSubscriber
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
         //Create a consumer and bind it to the queue
-        Consumer consumer = new DefaultConsumer(channel)
-        {
-            @Override
-            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
-                String message = new String(body, "UTF-8");
-                System.out.println(" [x] Received '" + message + "'");
-            }
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" + message + "'");
         };
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
 
-        // Start consuming messages from the queue
-        channel.basicConsume(QUEUE_NAME, true, consumer);
 
-        // Wait indefinitely until notified to exit
-        latch.await();
     }
 
 }
